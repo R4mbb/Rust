1cb76fc9cc0af32b0a01be655e816544bab182f7
R4mbb <yong010301@gmail.com>
2024-06-26 10:14:48 +0900

    2024-06-26

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -5,6 +5,18 @@ fn main() {
     another_function_2(5);
     print_labeled_measurement_1(5, 'h');
     print_labeled_measurement_2(5, "test");
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is {y}");
+    
+    let x = five();
+    println!("The value of x is {x}");
+
+    let x = plus_one(5);
+    println!("The value of x is {x}");
 }
 
 fn another_function_1() {
@@ -21,4 +33,12 @@ fn print_labeled_measurement_1(value: i32, unit_label: char) {
 
 fn print_labeled_measurement_2(value: i32, unit_label: &str) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
