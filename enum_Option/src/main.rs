42aa4694ecfdee34e06c48e208659a23c150b70f
R4mbb <yong010301@gmail.com>
2024-07-13 23:57:53 +0900

    2024-07-13

---
+++ enum_Option/src/main.rs
@@ -0,0 +1,18 @@
+enum Option<T> {
+    None,
+    Some(T),
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+    let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    
+    //let sum = x + y;
+
+}
--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,4 @@
+/*
 enum IpAddrKind {
     V4(String),
     V6(String),
@@ -8,6 +9,7 @@ struct IpAddr {
     address: String,
 }
 
+
 let home = IpAddr {
     kind: IpAddrKind::V4,
     address: String::from("127.0.0.1"),
@@ -17,12 +19,40 @@ let loopback = IpAddr {
     kind: IpAddrKind::V6,
     address: String::from("::1"),
 };
-
-fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-
-
+*/
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
-fn route(ip_kind: IpAddrKind) {}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+/*
+struct QuitMessage;
+struct MoveMessage { x: i32, y: i32 };
+struct WriteMessage(String);
+struct ChangeColorMessage(i32, i32, i32);
+*/
+impl Message {
+    fn call(&self) {
+        //
+    }
+}
+
+fn main() {
+    //let four = IpAddrKind::V4;
+    //let six = IpAddrKind::V6;
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call()
+}
+
+//fn route(ip_kind: IpAddrKind) {}
