ac3b5913845b87c0b2387a61ac7184b3a876b75f
R4mbb <yong010301@gmail.com>
2024-07-14 23:31:11 +0900

    2024-07-14

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,34 +1,66 @@
+trait Moveable {
+    fn move_to(&mut self, x: i32, y: i32);
+}
+
 struct Player {
     name: String,
     level: i32,
     hp: i32,
     mp: i32,
+    stamina: i32,
 }
+
+struct Pet {
+    name: String,
+    level: i32,
+    exp: i32,
+}
+
 impl Player {
     fn increase_level(&mut self) {
         self.level += 1;
         self.hp += 10;
         self.mp += 5;
+        self.stamina += 10;
+    }
+}
+
+impl Moveable for Player {
+    fn move_to(&mut self, x: i32, y: i32) {
+        if self.stamina <= 0 {
+            println!("Not enough stamina to move");
+            return;
+        }
+
+        self.stamina -= 1;
+        println!("Moving player to ({x}, {y})");
+    }
+}
+
+impl Moveable for Pet {
+    fn move_to(&mut self, x: i32, y: i32) {
+        self.exp += 1;
+        println!("Moving pet to ({x}, {y})");
     }
 }
 
 fn main() {
-    let mut player1 = Player {
-        name: String::from("Yang"),
-        level: 1,
-        hp: 100,
-        mp: 50,
-    };
+    let mut object = vec![
+        Box::new(Player {
+            name: String::from("Jaewon"),
+            level: 1,
+            hp: 100,
+            mp: 50,
+            stamina: 100,
+        }) as Box<dyn Moveable>,
+        Box::new(Pet {
+            name: String::from("Minjae"),
+            level: 1,
+            exp: 0,
+        }) as Box<dyn Moveable>,
+    ];
 
-    println!("Player: {}", player1.name);
-    println!("Level: {}", player1.level);
-    println!("HP: {}", player1.hp);
-    println!("MP: {}", player1.mp);
-
-    player1.increase_level();
-
-    println!("Player: {}", player1.name);
-    println!("Level: {}", player1.level);
-    println!("HP: {}", player1.hp);
-    println!("MP: {}", player1.mp);
+    for object in object.iter_mut() {
+        object.move_to(10, 20);
+    }
 }
